307cb9c488a2d576ea836ddac68c26067e8a0894
Miguel <miguelandradebarreto2@gmail.com>
2021-01-23 13:45:30 -0300

    :rocket: functional requests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-
-const app = express()
-app.use(express.json()) //? para todas as routes usar json no body
+const { uuid } = require('uuidv4')
 
 //! Métodos HTTP
 //? GET: Obter dados
@@ -22,29 +20,58 @@ app.use(express.json()) //? para todas as routes usar json no body
 //? como enviar para o back: {} JSON
 //? como receber do front: req.body
 
-app.get('/projects', (req, res) => {
-  const { title, owner} = req.query
-  console.log(title, owner)
+//! Repostas HTTP
+//? Respostas de informação (100-199),
+//? Respostas de sucesso (200-299),
+//? Redirecionamentos (300-399)
+//? Erros do cliente (400-499)
+//? Erros do servidor (500-599).
 
-  return res.json(['Projeto 1', 'Projeto 2'])
+const app = express()
+app.use(express.json()) //? para todas as routes usar json no body
+
+const projects = [] //? fake database
+
+app.get('/projects', (req, res) => {
+  const { title } = req.query
+  const results = title ? projects.filter(project => project.title.includes(title)) : projects
+
+  return res.json(results)
 })
 
 app.post('/projects', (req, res) => {
-  const body = req.body
-  console.log(body)
+  const { title, owner } = req.body
+  const project = { id: uuid(), title, owner }
 
-  return res.json(['Projeto 1', 'Projeto 2', 'Projeto 3'])
+  projects.push(project)
+  return res.json(project)
 })
 
 app.put('/projects/:id', (req, res) => {
   const { id } = req.params
-  console.log(id)
+  const { title, owner } = req.body
 
-  return res.json(['Projeto 1', 'Projeto 2', 'Projeto 4'])
+  const projectIndex = projects.findIndex(project => project.id === id)
+  if (projectIndex < 0) return res.status(400).json({ error: 'project not found' })
+
+  const project = {
+    id, title, owner
+  }
+
+  projects[projectIndex] = project
+
+  return res.json(project)
 })
 
 app.delete('/projects/:id', (req, res) => {
-  return res.json(['Projeto 1', 'Projeto 2'])
+  const { id } = req.params
+
+  const projectIndex = projects.findIndex(project => project.id === id)
+  if (projectIndex < 0) return res.status(400).json({ error: 'project not found' })
+
+  projects.splice(projectIndex, 1)
+
+  return res.status(204), send()
 })
 
 app.listen(3333, () => {
